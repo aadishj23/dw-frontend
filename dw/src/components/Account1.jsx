a54dd61287acaf3bdd543dab7735871bc8fa3843
Aadish Jain <aadishjain017@gmail.com>
2024-09-25 11:31:59 +0530

    completed 3 controllers

--- dw/src/components/Account1.jsx
+++ dw/src/components/Account1.jsx
@@ -88,9 +88,9 @@ function Account1() {
                   />
                   <input
                       type="text"
-                      placeholder="Enter bankName"
-                      name="bankName"
-                      value={formData.bankName}
+                      placeholder="Enter balance"
+                      name="balance"
+                      value={formData.balance}
                       onChange={handleChangeSignIn}
                       className="w-full px-3 py-2 mb-6 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
                       required
